687ac34079fc84a93acbeec6e5115730f238fb80
Jahanvi <jahanvimp128@gmail.com>
2023-10-06 20:36:57 +0530

    server created

---
+++ App.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+// app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/hack4in",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+})
+
+const PORT = 3001
+app.listen(PORT,() => {
+    console.log("server is running on port ",PORT);
+})
